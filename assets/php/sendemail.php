b1abee17126207480fccd5a7b6539bbab3cdaeeb
noxor0 <avaterq0@gmail.com>
2018-04-03 14:08:33 -0700

    More Php fixes

--- assets/php/sendemail.php
+++ assets/php/sendemail.php
@@ -4,13 +4,18 @@
 	$email = @trim(stripslashes($_POST['email']));
 	$subject = @trim(stripslashes($_POST['subject']));
 	$message = @trim(stripslashes($_POST['message']));
+	$headers = array("From: from@example.com",
+	    "Reply-To: reachout-noreply@noxor.io",
+	    "X-Mailer: PHP/" . PHP_VERSION
+	);
+	$headers = implode("\r\n", $headers);
 
 	$email_from = $email;
 	$email_to = 'jobs@noxor.io';
 
 	$body = 'Name: ' . $name . "\n\n" . 'Email: ' . $email . "\n\n" . 'Subject: ' . $subject . "\n\n" . 'Message: ' . $message;
 
-	$success = mail($email_to, $body, 'Name: ' . $name . "\n\n" . 'Email: ' . $email . "\n\n" . 'Subject: ' . $subject . "\n\n" . 'Message: ' . $message);
+	mail($email_to, $subject, $body, $headers);
 
 ?>
 
@@ -18,5 +23,5 @@
 <html lang="en-US">
 <head>
 	<script>alert("Thanks for reaching out!");</script>
-	<!-- <meta HTTP-EQUIV="REFRESH" content="0; url=http://noxor.io"> -->
+	<meta HTTP-EQUIV="REFRESH" content="0; url=http://noxor.io">
 </head>
